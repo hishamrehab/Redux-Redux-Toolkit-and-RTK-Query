8eda92bb8c07c4c41c36128afa8b5b40f2a93826
hishamrehab <hishamrehab30@gmail.com>
2026-01-05 01:36:05 +0200

    Implement Store Responsibilites

--- 1-  Redux/script.js
+++ 1-  Redux/script.js
@@ -1,3 +1,8 @@
+const redux= require("redux");
+const createStore = redux.createStore;
+
+
+// Action Type
 const ORDER_PIZZA = "ORDER_PIZZA"
 
 // Action 
@@ -7,7 +12,7 @@ const ORDER_PIZZA = "ORDER_PIZZA"
 // }
 
 
-// Action Creator
+// Action Creator 
 function orderPizza() {
     return {
     type : ORDER_PIZZA,
@@ -26,7 +31,7 @@ const reducer = (state = initialState , action) => {
     case ORDER_PIZZA: 
     return {
         ...state,
-        pizzaBase: pizzaBase - 1
+        pizzaBase: state.pizzaBase - 1
     }
     default:
     return state
@@ -34,3 +39,18 @@ const reducer = (state = initialState , action) => {
 }
 
 
+// STORE
+const store = createStore(reducer);
+
+// 2-It exposes a method calld getState whick gives your application access to the current state im the store
+
+
+console.log("Initial State", store.getState());
+
+// 3 - Register linteners cia subscribe(listener)
+const unsubscribe = store.subscribe(() => console.log("Updated State", store.getState()));
+store.getState();
+// 4 - Allows state to be updated via dispatch(action)
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+unsubscribe();
