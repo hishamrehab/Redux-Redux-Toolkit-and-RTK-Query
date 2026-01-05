39f312d9ceb5bb8233fa9d73e6acd8da0ab1488c
hishamrehab <hishamrehab30@gmail.com>
2026-01-06 00:33:51 +0200

    use Dispatch Hook

--- 2 -  React Redux/react-redux/src/HooksContainer.jsx
+++ 2 -  React Redux/react-redux/src/HooksContainer.jsx
@@ -1,12 +1,16 @@
-import { useSelector } from "react-redux"
+import { useSelector , useDispatch } from "react-redux"
+import { orderPizza } from "./redux";
 
 function HookContainer() {
    const pizzaBase = useSelector(state => state.pizzaBase);
+   const dispatch = useDispatch();
    console.log(pizzaBase);
+
+
   return (
     <div className='container'>
         <h2 className="text">Number of Pizza Base available - {pizzaBase}</h2>
-        <button className="btn">Order Pizza</button>
+        <button className="btn" onClick={() => dispatch(orderPizza())}>Order Pizza</button>
     </div>
   )
 }
