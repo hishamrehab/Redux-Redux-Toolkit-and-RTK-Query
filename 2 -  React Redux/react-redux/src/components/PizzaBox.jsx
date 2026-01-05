bd4dac2cd57e3a9da67026ee8305320192d6c78f
hishamrehab <hishamrehab30@gmail.com>
2026-01-05 23:50:03 +0200

    Connect Function

--- 2 -  React Redux/react-redux/src/components/PizzaBox.jsx
+++ 2 -  React Redux/react-redux/src/components/PizzaBox.jsx
@@ -1,30 +1,26 @@
 import { orderPizza  }from "../redux"
-const PizzaBox = (props) => {
-  console.log("props", props);
+import { connect } from "react-redux"
 
+function PizzaBox(props) {
+    console.log(props);
+    
   return (
-    <div className="container">
-      <h2 className="text">
-        Number of Pizza Base available - 100
-      </h2>
-      <button className="btn">
-        Order Pizza
-      </button>
+    <div className='container'>
+        <h2 className="text">Number of Pizza Base available - {props.pizzaBase}</h2>
+        <button className="btn" onClick={props.orderPizza}>Order Pizza</button>
     </div>
   )
 }
 
-const mapStateToProps = (state) => {
-   return {
-   pizzaBase:state.pizzaBase
-   }
+const mapStateToProps=(state)=>{
+    return{
+        pizzaBase:state.pizzaBase
+    }
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    orderPizza: () => dispatch(orderPizza())
-  }
+const mapDispatchToProps=(dispatch)=>{
+    return{
+      orderPizza :()=>  dispatch(orderPizza())
+    }
 }
-
-
-export default PizzaBox
+export default connect(mapStateToProps,mapDispatchToProps)(PizzaBox);
